185b3280b8ab5255d1e3c5f0596865d3ef6dd524
Inna <inna.kachinskaya13@gmail.com>
2021-07-23 01:00:05 +0300

    Add push

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-function MyArrayProto() {}
+function MyArrayProto() {
+  this.push = function () {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  };
+}
 
 function MyArray() {
   this.length = 0;
@@ -6,3 +13,6 @@ function MyArray() {
 MyArray.prototype = new MyArrayProto();
 
 const myArray = new MyArray();
+
+myArray.push(0, 1, 2, 3, 4, 5, 6);
+console.log(myArray);
